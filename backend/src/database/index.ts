6361c223bcad9b88b9d060291159064e856d4d87
Mimi Leon <mireyaleon76@gmail.com>
2025-07-10 13:16:54 -0700

    Update index.ts

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI || 'mongodb://127.0.0.1:27017/hsdla');
-    console.log('Connected to MongoDB:', process.env.DB_URI);
+    console.log('Connected to MongoDB:');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1); 
